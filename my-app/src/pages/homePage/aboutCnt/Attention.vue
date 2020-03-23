725b6ca0ca6d258ec06cc5c71614021c275faf0e
hejiazhe <hejiazhe@qq.com>
2020-03-23 17:18:57 +0800

    关注列表完成

--- my-app/src/pages/homePage/aboutCnt/Attention.vue
+++ my-app/src/pages/homePage/aboutCnt/Attention.vue
@@ -2,12 +2,22 @@
  * @Autor: hjz
  * @Date: 2020-03-23 16:03:12
  * @LastEditors: hjz
- * @LastEditTime: 2020-03-23 16:18:45
+ * @LastEditTime: 2020-03-23 17:05:44
  * @Description: 
  -->
 <template>
-  <div class="CmtsCnt_wrapper">
+  <div class="Attention_wrapper">
     <h2>{{ isAttention ? "关注列表":"粉丝列表"}}</h2>
+    <ul class="menber_list">
+      <li class="list_item" v-for="(item,index) in menber_list" :key="index">
+        <div class="item_wrapper">
+          <img class="pic_img" :src="item.picture" alt />
+          <span class="name">{{ item.name }}</span>
+          <img class="gender_img" :src="item.gender == 1? maleImg:femaleImg" alt />
+          <img class="vip_img" v-show="item.vip" :src="vipImg" alt />
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -16,7 +26,42 @@ export default {
   components: {},
   data() {
     return {
-      isAttention: false
+      isAttention: false,
+      menber_list: [
+        {
+          name: "孟德-曹操",
+          picture: require("../../../assets/img/home/aboutCnt/attention/pic1.jpg"),
+          vip: true,
+          gender: 1
+        },
+        {
+          name: "仲达-司马懿",
+          picture: require("../../../assets/img/home/aboutCnt/attention/pic2.jpg"),
+          vip: true,
+          gender: 0
+        },
+        {
+          name: "文和-贾诩",
+          picture: require("../../../assets/img/home/aboutCnt/attention/pic3.jpg"),
+          vip: true,
+          gender: 1
+        },
+        {
+          name: "文若-荀彧",
+          picture: require("../../../assets/img/home/aboutCnt/attention/pic4.jpg"),
+          vip: false,
+          gender: 2
+        },
+        {
+          name: "奉孝-郭嘉",
+          picture: require("../../../assets/img/home/aboutCnt/attention/pic5.jpg"),
+          vip: true,
+          gender: 1
+        }
+      ],
+      maleImg: require("../../../assets/img/home/aboutCnt/attention/male.png"),
+      femaleImg: require("../../../assets/img/home/aboutCnt/attention/female.png"),
+      vipImg: require("../../../assets/img/home/aboutCnt/attention/vip.png")
     };
   },
   methods: {},
@@ -30,7 +75,55 @@ export default {
 </script>
 
 <style scoped lang="less">
-.CmtsCnt_wrapper {
-  height: 200vh;
+.Attention_wrapper {
+  h2 {
+    width: 100%;
+    font-size: 24px;
+    padding: 14px 0 4px;
+    padding-left: 14px;
+    color: #141414;
+    letter-spacing: 2px;
+    // text-align: center;
+    font-weight: normal;
+  }
+  .menber_list {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-items: center;
+    // align-items: center;
+  
+    .list_item {
+      flex: 0 0 auto;
+      margin: 4px 0;
+      padding: 14px 14px;
+      background-color: #fff;
+      width: 100%;
+      .item_wrapper {
+        display: flex;
+        justify-items: center;
+        align-items: center;
+        img.pic_img {
+          width: 44px;
+          height: 44px;
+        }
+        span.name {
+          font-size: 17px;
+          color: #141414;
+          letter-spacing: 1px;
+          padding: 0 13px;
+        }
+        img.gender_img {
+          width: 24px;
+          height: 24px;
+        }
+        img.vip_img {
+          width: 24px;
+          height: 24px;
+        }
+      }
+    }
+  }
 }
 </style>
